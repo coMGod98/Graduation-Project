5bc1866446225269f4f8f6e42f07ff5562ca385c
apvmf654 <tnvjwns@naver.com>
2023-10-21 12:41:58 +0900

    서버 연동 코드 추가

--- LookLook-WebApplication/src/pages/main.jsx
+++ LookLook-WebApplication/src/pages/main.jsx
@@ -5,11 +5,21 @@ import HotList from "../components/hotList";
 import NewList from "../components/newList";
 import styles from "./main.module.css";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Main() {
+
+  const [msg, setMsg] = useState([]);
+  useEffect(() => {
+    fetch("/api/hello")
+        .then((res) => {return res.json();})
+        .then((data) => {setMsg(data);})
+  }, []);
+  
   return (
     <>
       <Header />
+      {msg.map((content, idx) => <li key={`${idx} - ${content}`}>{content}</li>)}
       <Banner />
       <div className={styles.mainSection}>
         <div className={styles.mainHeader}>인기 상품</div>
