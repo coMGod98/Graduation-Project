f7fb6f8836b0da0aeb120a0a3638187a6cc4baee
suran28 <chocomilk2638@gmail.com>
2023-11-03 07:50:45 +0900

    update

--- LookLook-SpringApplication/demo/src/main/resources/data.sql
+++ LookLook-SpringApplication/demo/src/main/resources/data.sql
@@ -1,21 +1,21 @@
 -- 회원 테이블에 관리자 계정 1번으로
-INSERT INTO USERS (USERNAME, USERID, USERPW, AUTHORITY)
+INSERT INTO users (username, userid, userpw, authority)
 VALUES ('admin', 'admin0000', '$2a$10$Naqxf0wgmdyA9Q4YHZaeM.Fwwp14ZlN3YOfwY92Gi9v9NQyEQ/yj.', 'ROLE_ADMIN');
 
-INSERT INTO USERS (USERNAME, USERID, USERPW, AUTHORITY, EMAIL, PNUMBER, SEX, ADDRESS)
+INSERT INTO users (username, userid, userpw, authority, email, pnumber, sex, address)
 VALUES ('testuser', 'testuser0000', '$2a$10$Naqxf0wgmdyA9Q4YHZaeM.Fwwp14ZlN3YOfwY92Gi9v9NQyEQ/yj.', 'ROLE_USER', 'test123@looklook.com', '01012345678', 'MALE', '경기도 성남시');
 
 
 
 -- 상품 테이블
-INSERT INTO ITEM(ITEM_NAME, CATEGORY, STOCK, PRICE, PGENDER, ITEM_DETAIL, uid)
+INSERT INTO item(item_name, category, stock, price, pgender, item_detail, uid)
 VALUES ('샘플 상품 1','101',3, 10000, 'MAN', '상품 설명', 2);
-INSERT INTO ITEM(ITEM_NAME, CATEGORY, STOCK, PRICE, PGENDER, ITEM_DETAIL, uid)
+INSERT INTO item(item_name, category, stock, price, pgender, item_detail, uid)
 VALUES ('샘플 상품 2','201',5, 21000, 'WOMAN', '상품 설명', 2);
 
 
 -- -- 상품 사이즈
-INSERT INTO ITEM_SIZE (size_Name, product_id)
+INSERT INTO item_size (size_Name, product_id)
 VALUES
     ('S', 1),
     ('M', 1),
@@ -23,7 +23,7 @@ VALUES
     ('L', 2);
 --
 -- -- 상품 색상
-INSERT INTO ITEM_Color (color, product_id)
+INSERT INTO item_color (color, product_id)
 VALUES
     ('black', 1),
     ('white', 1),
