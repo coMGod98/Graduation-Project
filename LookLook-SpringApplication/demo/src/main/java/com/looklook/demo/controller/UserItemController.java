77cf88bae6f2a2c015fa5740ed317dc37cc8e0b2
leeyourimm <yourim1219@gachon.ac.kr>
2023-11-07 21:03:11 +0900

    Revert "//"
    This reverts commit 7829d41fe93e67e978c7236d5348036aea3447a0.

--- LookLook-SpringApplication/demo/src/main/java/com/looklook/demo/controller/UserItemController.java
+++ LookLook-SpringApplication/demo/src/main/java/com/looklook/demo/controller/UserItemController.java
@@ -5,16 +5,13 @@ import com.looklook.demo.domain.ItemColor;
 import com.looklook.demo.domain.ItemSize;
 import com.looklook.demo.dto.ItemDto;
 import com.looklook.demo.dto.UserResponseDto;
-import com.looklook.demo.repository.ItemRepository;
 import com.looklook.demo.service.UserItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,9 +22,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class UserItemController {
     private final UserItemService userItemService;
-
-    @Autowired
-    private ItemRepository itemRepository;
     // 카테고리 별 상품 조회
     @GetMapping("/category/{category}")
     public ResponseEntity<List<ItemDto>> showItemsByCategory(@PathVariable String category){
@@ -45,13 +39,4 @@ public class UserItemController {
         ItemDto itemDto = userItemService.getItemByPid(Long.valueOf(pid));
         return ResponseEntity.ok(itemDto);
     }
-
-    //상품 검색
-    //한글자만 쳐도 해당 글자가 있는 상품명 나오게 변경
-    @GetMapping("/product/search")
-    public ResponseEntity<List<Item>> searchProduct(@RequestParam("itemName") String itemName) {
-        List<Item> items = itemRepository.findByItemName(itemName);
-        return new ResponseEntity<>(items, HttpStatus.OK);
-    }
-
 }
--- LookLook-SpringApplication/demo/src/main/java/com/looklook/demo/repository/ChatBotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.looklook.demo.repository;
-
-import com.looklook.demo.domain.ChatBot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatBotRepository extends JpaRepository<ChatBot, Long> {
-}
